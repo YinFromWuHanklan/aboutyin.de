000554eddae837b0bba8fa1f67c01e99d8c395ed
Yin Yin Wu-Hanke <yinyinwu-hanke@Air-von-Yin.fritz.box>
2023-03-23 10:51:15 +0100

    navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,70 +1,44 @@
-import React, { useState } from "react"
-import styled from "styled-components";
-import { AnchorLink } from "gatsby-plugin-anchor-links";
-import "@fontsource/bebas-neue"
-import { FaHamburger } from "react-icons/fa";
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-const MainNav = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: var(--background-dark);
-    position: fixed;
-    height: 60px;
-`
+function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
 
-const NavLogo = styled.h1`
-    font-family: "Bebas Neue";
-`
+  const toggle = () => setIsOpen(!isOpen);
 
-const NavUl = styled.ul`
-    list-style-type: none;
-    display: flex;
-    justify-content: right;
-
-    @media (max-width: 600px) {
-      flex-direction: column;
-    }
-`
-
-const NavLi = styled.li`
-    padding: 10px 20px;
-    font-family: "Bebas Neue";
-`
-
-const BurgerMenu = styled.button`
-  svg {
-    fill: var(--secondary);
-  }
-
-  display: none;
-
-  @media (max-width: 600px) {
-    display: block;
-  }
-`
-
-const NavBar = () => {
-  const [showNavbar, setShowNavbar] = useState(false)
-
-  const handleShowNavbar = () => {
-    setShowNavbar(!showNavbar)
-  }
-    return (
-      <MainNav>
-        <div>
-          <NavLogo>ABOUTYIN</NavLogo>
-        </div>
-        <BurgerMenu onClick={handleShowNavbar}><FaHamburger /></BurgerMenu>
-        <NavUl className={showNavbar}>
-            <NavLi><AnchorLink to="#aboutme" title="About me">About me</AnchorLink></NavLi>
-            <NavLi><AnchorLink to="#skills" title="Skills">Skills</AnchorLink></NavLi>
-            <NavLi><AnchorLink to="#projekte" title="Projekte">Projekte</AnchorLink></NavLi>
-            <NavLi><AnchorLink to="#kontakt" title="Kontakt">Kontakt</AnchorLink></NavLi>
-        </NavUl>
-      </MainNav>
-    )
+  return (
+    <div>
+      <Navbar expand="sm" container="fluid" fixed="top">
+        <NavbarBrand href="/">aboutyin</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar className='justify-content-end'>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="#aboutme">About me</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#skills">Skills</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#projekte">Projekte</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#kontakt">Kontakt</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-
-export default NavBar
+export default NavBar;
